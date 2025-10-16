5edb8f1c672d2d6c9c6e2889cac3ab8d7204f36e
ffm <ligerxrobo@gmail.com>
2025-10-16 10:28:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -57,7 +57,7 @@ async def start_command(client: Client, message: Message):
 
             short_photo = client.messages.get("SHORT_PIC", "")
             short_caption = client.messages.get("SHORT_MSG", "")
-            tutorial_link = getattr(client, 'tutorial_link', "https://t.me/How_to_Download_7x/26")
+            tutorial_link = getattr(client, 'tutorial_link', "https://t.me/how_to_open_link_ak/16")
 
             await client.send_photo(
                 chat_id=message.chat.id,
@@ -69,7 +69,7 @@ async def start_command(client: Client, message: Message):
                         InlineKeyboardButton("ᴛᴜᴛᴏʀɪᴀʟ •", url=tutorial_link)
                     ],
                     [
-                        InlineKeyboardButton(" • ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ •", url="https://t.me/Premium_Fliix/21")
+                        InlineKeyboardButton(" • ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ •", url="http://t.me/AK_ownerbot")
                     ]
                 ])
             )
