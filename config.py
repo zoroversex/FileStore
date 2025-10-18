9ec093493477bd6bf709e0873dfc9a1df2fef781
ffm <ligerxrobo@gmail.com>
2025-10-18 17:39:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ SHORT_TUT = "https://t.me/how_to_open_link_ak"
 
 # Bot Configuration
 SESSION = "yato"
-TOKEN = "77956OnF0OkY"
+TOKEN = ""
 API_ID = "20881061"
 API_HASH = "5efa353cefe189620d28dcb30f0fea20"
 WORKERS = 5
 
-DB_URI = "mongodb+srv://"
+DB_URI = ""
 DB_NAME = "yato"
 
 FSUBS = [[-1001785093771, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
